3bc23d56febb81041349988898ccc9013bc85143
Viet Le <lttviet@gmail.com>
2025-01-01 22:10:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Website
+
+This website is built using [Docusaurus].
+The live website is [here].
+
+  [Docusaurus]: https://docusaurus.io/
+  [here]: https://lttviet.com
+
+## Installation
+
+```bash
+pnpm install
+```
+
+## Local Development
+
+```bash
+pnpm start
+```
+
+This command starts a local development server and opens up a browser window.
+Most changes are reflected live without having to restart the server.
+
+## Build
+
+```bash
+pnpm build
+```
+
+This command generates static content into the `build` directory
+and can be served using any static contents hosting service.
---
+++ docusaurus.config.ts
@@ -0,0 +1,98 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Thought Machine',
+  tagline: 'Random thoughts',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://lttviet.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'lttviet', // Usually your GitHub org/user name.
+  projectName: 'lttviet.com', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/lttviet/lttviet.com/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          exclude: [
+            'README.md',
+          ]
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Thought Machine',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tech',
+        },
+        {
+          href: 'https://github.com/lttviet/lttviet.com',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    docs: {
+      sidebar: {
+        hideable: true
+      },
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['bash']
+    },
+    algolia: {
+      appId: '4R3IFQCE2A',
+      apiKey: '9321ef9aaac72d1fb02c0bb997b9092c',
+      indexName: 'lttviet',
+    }
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
